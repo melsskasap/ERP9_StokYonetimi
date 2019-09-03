59a9c104a0e1ff7c8d2ae3d86a9fe43acd15ed8e
melis kasap <meliskasap95@gmail.com>
2019-09-03 15:06:47 +0300

    repo ok

---
+++ Sy.Core/Abstracts/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sy.Core.Abstracts
+{
+    public interface IRepository<TEntity,TKey> where TEntity : IEntity<TKey>   //where yazarak sınırlandırıyoruz sadece TEntity yazabilirz.
+    {
+        TEntity GetById(TKey id);
+        TKey Insert(TEntity entity);
+        int Update(TEntity entity);
+        int Delete(TEntity entity);
+        int Save();
+        IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> predicate = null);
+
+    }
+}
